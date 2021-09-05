26a6a98574bdb327648d5191b8c327bf3c62c0de
yADGithub <2007andredoe@gmail.com>
2021-09-05 18:59:06 -0300

    sem favicon

--- pages/_app.js
+++ pages/_app.js
@@ -23,7 +23,6 @@ function MyApp({Component, pageProps}) {
     <meta content="Olá! Sou a zuly, um bot brasileiro focado em anime para o público otaku, com sistemas que bots de anime gringos tem, porém sou 100% brasileira, ou seja, suporte mais fácil e melhor interação!" property="og:description"/>
     <meta content="ZulyBot" property="og:title"/>
     <meta content="600" property="og:ttl"/>
-    <link rel="shortcut icon" href="/img/favicon.png" type="image/x-icon"/>
     <script async src="https://arc.io/widget.min.js#dLLm1Tz6"></script>
     <script src="//code.tidio.co/ognomdog2y07i6p8ebctb3sqzb6ermmo.js" async></script>
     </Head>
